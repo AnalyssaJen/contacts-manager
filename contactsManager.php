d9112de5c99c73fb48b9600b340469eae81d2066
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-07-28 13:39:36 -0500

    Got the search working

--- contactsManager.php
+++ contactsManager.php
@@ -1,27 +1,27 @@
 <?php
-
-//function that will make the machine work
-function viewContacts($filename){
-	//newcontacts can be added to this array
+function getAllContacts($filename){
 	$contacts = array();
 	$handle = fopen($filename, 'r');
 	$contactString = trim(fread($handle, filesize($filename)));
 	$contactsArray = explode("\n", $contactString);
-	
-	// print_r($contactsArray);
-
-	foreach ($contactsArray as $contact) 
-	{	
+	return $contactsArray;
+}
+//function that will make the machine work
+function viewContacts($contacts){
+	$contactList = array();
+	foreach ($contacts as $contact){	
 		$infoArray = explode("|", $contact);
 		$parsedContacts =[];
 		$parsedContacts['name'] = $infoArray[0];
 		$parsedContacts['number'] = $infoArray[1];
-		array_push($contacts, $parsedContacts);	
+		array_push($contactList, $parsedContacts);	
 	}
-	foreach($contacts as $contact){
+	print_r("-------------CONTACTS-------------" . PHP_EOL . "    Name        |   Phone Number  " . PHP_EOL . "----------------------------------" . PHP_EOL);
+	// viewContacts("contacts.txt");
+	foreach($contactList as $contact){
 		print_r($contact['name'] . '    |     ' .$contact['number'] .PHP_EOL );
 	}
-return $contacts;
+	
 }
 
 //basic beginning template
@@ -32,17 +32,15 @@ return $contacts;
 // //makes the entire menu work
 function menu(){
 
-
-	print_r("-------------CONTACTS-------------" . PHP_EOL . "    Name        |   Phone Number  " . PHP_EOL . "----------------------------------" . PHP_EOL);
-	viewContacts("contacts.txt");
 	print_r(PHP_EOL . PHP_EOL);
 	print_r("1. View Contacts" . PHP_EOL . "2. Add Contact" . PHP_EOL . "3. Search Contact By Name" . PHP_EOL . "4. Delete Contact" . PHP_EOL . "5. Exit" . PHP_EOL);
-
+	
 	fwrite(STDOUT, 'Please enter a # to select an action' . PHP_EOL);
 	$input = trim(fgets(STDIN));
 
 	if ($input == 1) {
-		viewContacts('contacts.txt');
+		$contacts = getAllContacts('contacts.txt');
+		viewContacts($contacts);
 	}
 		elseif ($input == 2) {
 		addContact();
@@ -69,8 +67,6 @@ menu();
 
 function addContact(){
 
-	$newContact = [];
-
 	fwrite(STDOUT, 'Please enter new name' . PHP_EOL);
 	$name = trim(fgets(STDIN));
 	
@@ -78,34 +74,38 @@ function addContact(){
 	fwrite(STDOUT, 'Please enter new phone number' . PHP_EOL);
 	$number = trim(fgets(STDIN));
 
-	// $newContact['name'] = $name;
-	// $newContact['number'] = $number;
-	
-	// array_push($contacts,$newContact);
-
 	$filename = 'contacts.txt';
 	$handle = fopen($filename, 'a');
 	fwrite($handle, $name . "|" . $number);
 	fclose($handle);
-
 }
 
 
 function searchContacts(){
+	//Taking from user the Search name
 	fwrite(STDOUT, 'Please enter a contact name to View' . PHP_EOL);
 	$searchName = trim(fgets(STDIN));
-	$nameFound = (array_search('searchName', $contractString));
- 	print_r(array_search('searchName', $contactString));
+	$contacts = [];
 	
+	$contactsArray = getAllContacts('contacts.txt');
+	
+	foreach($contactsArray as $contact){
+		if (strpos($contact, $searchName)!==false){
+			array_push($contacts, $contact);
+		}
+
+	}
+	viewContacts($contacts);
+
 }
+
+
+
+
+
 //function deleteContact(){
 // 	if ($input ==4){
 //		echo "You have deleted $contact" . PHP_EOL;		
-// 	}
-// }
-// function exit(){
-// 	if ($input ==5){
-// 		exit;
 // 	}
 // }
 
